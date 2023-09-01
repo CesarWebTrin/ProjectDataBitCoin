84f47aaed2d7da6e07a2a273f3ca02b92ddeaea7
CesarWebTrin <cesinhart@gmail.com>
2023-09-01 02:16:45 -0300

    MODELO DE DADOS

---
+++ ModeloDeDados.sql
@@ -0,0 +1,10 @@
+CREATE TABLE BitCoin (
+	PriceOpen decimal(10,2),
+	PriceHigh decimal(10,2),
+	PriceLow decimal(10,2), 
+	PriceClose decimal(10,2),
+	PeriodStart datetime, 
+	PeriodEnd datetime
+)
+
+SELECT * FROM BitCoin
